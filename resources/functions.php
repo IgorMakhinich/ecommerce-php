b2f7eecd6cad31cc34eee47aec8c0b21b3c60ce3
Igor Makhinich <igor.makhinich@gmail.com>
2021-01-05 14:48:56 +0200

    send_message function

--- resources/functions.php
+++ resources/functions.php
@@ -188,7 +188,22 @@ function login_user(){
 
 function send_message(){
 	if(isset($_POST['submit'])){
-		echo "It works";
+		$to = "someEmailaddres@gmail.com";
+		$from_name = $_POST['name'];
+		$subject = $_POST['subject'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+
+		$headers = "From: {$from_name} {$email}";
+
+		$result = mail($to, $subject, $message, $headers);
+
+		if(!$result){
+			set_message("Sorry we could not send your message");
+			redirect("contact.php");
+		} else {
+			set_message("Your message has been send");
+		}
 	}
 }
 
